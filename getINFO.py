c76d8b73827de7c661f7b3330a712301ca8cffad
CuciureanuDan <dancuciureanu4@yahoo.com>
2024-03-23 18:54:42 +0200

    Update GUI and introduced text check feature

--- getINFO.py
+++ getINFO.py
@@ -1,48 +1,86 @@
-
 from geopy.geocoders import Nominatim
 from timezonefinder import TimezoneFinder 
 from datetime import datetime
 import pytz
 import requests
+import re
+
+def checkText(text):
+    """
+    Check if the input text is valid.
+
+    Args:
+    text (str): The input text to be checked.
+
+    Returns:
+    bool: True if the text is valid, False otherwise.
+    """
+    if len(text) < 2 or not re.match(r'^[a-zA-Z\s]+$', text):
+        return False
+    return True
 
 
-def getWeather(city, clock_widget, timezone_widget):
-    
-    geolocator = Nominatim(user_agent="getINFO")
-    location = geolocator.geocode(city)
+def getWeather(city, clock_widget, timezone_widget,weather_labels):    
+    """
+    Get weather information for a given city and update the GUI widgets.
 
-    obj = TimezoneFinder()
-    result = obj.timezone_at(lng=location.longitude,lat=location.latitude)
-    timezone_widget.config(text = result)
-    
-    home = pytz.timezone(result)
-    local_time = datetime.now(home)
-    current_time = local_time.strftime("%I:%M %p")
+    Args:
+    city (str): Name of the city.
+    clock_widget (Widget): Clock widget to be updated.
+    timezone_widget (Widget): Timezone widget to be updated.
+    weather_labels (list): List of weather labels to be updated.
+    """
 
-    clock_widget.config(text=current_time)
+    if checkText(city):
+        geolocator = Nominatim(user_agent="getINFO")
+        try:
+            location = geolocator.geocode(city)
+        except Exception as e:
+            weather_labels[0].config(text=f"Error in getting the location: {e}")
+            for idx in range(1,5):
+                weather_labels[idx].config(text='')
+            clock_widget.config(text='')
+            timezone_widget.config(text ='')
+            return
 
-    lat =str( location.latitude)
-    lon = str(location.longitude)
+        obj = TimezoneFinder()
+        result = obj.timezone_at(lng=location.longitude,lat=location.latitude)
+        timezone_widget.config(text = result)
+        
+        home = pytz.timezone(result)
+        local_time = datetime.now(home)
+        current_time = local_time.strftime("%I:%M %p")
 
-    #api= "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={db8271986daa8d8578383752c3ae182e}"
-    api = "https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}".format(lat, lon, "db8271986daa8d8578383752c3ae182e")
+        clock_widget.config(text=current_time)
 
+        lat =str( location.latitude)
+        lon = str(location.longitude)
 
+        api = "https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}".format(lat, lon, "db8271986daa8d8578383752c3ae182e")
+        json_data = requests.get(api).json()
 
-    json_data = requests.get(api).json()
+        #current info about weather
 
+        temp = round( json_data['main']['temp'] - 273.15 ,1) # transforming in celsius and round it to one decimal
+        humidity = json_data['main']['humidity']
+        wind = json_data['wind']['speed']
+        description = json_data['weather'][0]['description']
 
-    #current 
+        # Update weather labels
+        weather_labels[0].config(text=f"The weather is for city: {city}.")
+        weather_labels[1].config(text=f"Temperature: {temp}°C")
+        weather_labels[2].config(text=f"Humidity: {humidity}%")
+        weather_labels[3].config(text=f"Wind Speed: {wind} m/s")
+        weather_labels[4].config(text=f"Description: {description}")
 
-    temp = round( json_data['main']['temp'] - 273.15 ,1) # transforming in celsius and round it to one decimal
-    humidity = json_data['main']['humidity']
-    wind = json_data['wind']['speed']
-    description = json_data['weather'][0]['description']
-
-    print(temp)
-    print(humidity)
-    print(wind)
-    print(description)
+        for label in weather_labels:
+            label.place()
+    else:
+        weather_labels[0].config(text=f"Wrong city name.")
+        for idx in range(1,5):
+            weather_labels[idx].config(text='')
+        clock_widget.config(text='')
+        timezone_widget.config(text ='')
 
 
     
--- weatherAppGUI.py
+++ weatherAppGUI.py
@@ -1,11 +1,5 @@
 from tkinter import *
 import tkinter as tk
-from geopy.geocoders import Nominatim
-from timezonefinder import TimezoneFinder 
-from datetime import *
-import pytz
-from PIL import Image, ImageTk
-
 from getINFO import getWeather
 
 # Create a window
@@ -15,52 +9,36 @@ root.geometry("890x470+300+300")  # The format is width x height + x_offset + y_
 root.configure(bg="#747474")
 root.resizable(False, False)  # Window not resizable
 
-# Round_box = PhotoImage(file="")
+# Calculate the center coordinates of the window
+window_center_x = root.winfo_reqwidth() // 2
+window_center_y = root.winfo_reqheight() // 2
 
-Label(root, bg="#707070").place(x=30, y=110)
 
-# Labels for weather information
-labels_text = ["Temperature", "Humidity", "Pressure", "Wind Speed", "Description"]
-for idx, text in enumerate(labels_text):
-    Label(root, text=text, font=("Arial", 11), bg="#747474").place(x=50, y=120 + (idx * 20))
+Label(root, bg="#707070").place(x=100, y=110)
+
+weather_labels = []  # List to store label widgets for weather information
+LabelAux = Label(root,text = None, font=("Arial", 17), bg="#747474")
+LabelAux.place(x=window_center_x, y=250)
+weather_labels.append(LabelAux)
+#LabelAux.grid_remove()  #  it is hided first 
+
+for idx in range(4):
+    label = Label(root,text = None, font=("Arial", 15), bg="#747474")
+    label.place(x=window_center_x, y=300 + (idx * 30))
+    weather_labels.append(label)
+    #label.grid_remove() # initially hide the labels
+
+
+# Label for the instruction
+instruction_label = Label(root, text="Enter the city and press enter:", font=("poppins", 15), bg="#747474", fg="white")
+instruction_label.place(x=window_center_x + 80, y=window_center_y)
 
 # Search box
-textfield = Entry(root, justify='center', width=15, font=("poppins", 25, 'bold'), bg="#444444", border=0)
-textfield.place(x=370, y=130)
+textfield = Entry(root, justify='center', width=30, font=("poppins", 25, 'bold'), bg="#444444", border=0)
+textfield.place(x=window_center_x +80 , y=window_center_y + 40 )
 textfield.focus()  # Focus by default
 # Bind the Enter key to call the function
-textfield.bind("<Return>", lambda event: getWeather(textfield.get(), clock, timezone))
-
-# Button boxes
-frame = Frame(root, width=900, height=180, bg="#747474", border=0)
-frame.pack(side=BOTTOM)
-
-# Create a Canvas widget inside the frame
-canvas = tk.Canvas(frame, width=900, height=180, bg="#747474", border=0)
-canvas.pack()
-
-# Define the number of rectangles and spacing
-num_rectangles = 7
-spacing = 10
-
-# Calculate the width of each rectangle, including spacing
-rect_width = (880 - (num_rectangles + 1) * spacing) / num_rectangles
-
-# Position of the rectangles vertically
-y1 = 30
-y2 = 180 - 30  # Height of the frame minus spacing from top and bottom
-
-# Define initial x-coordinate
-x1 = spacing
-
-# Draw seven rectangles
-for _ in range(num_rectangles):
-    # Calculate x2 based on x1 and rect_width
-    x2 = x1 + rect_width
-    # Create the rectangle on the canvas
-    canvas.create_rectangle(x1, y1, x2, y2, fill="#BEAFC2")
-    # Update x-coordinate for the next rectangle
-    x1 = x2 + spacing
+textfield.bind("<Return>", lambda event: getWeather(textfield.get(), clock, timezone,weather_labels))
 
 #clock
 
@@ -71,10 +49,6 @@ clock.place(x=30,y=20)
 
 timezone=Label(root,font=("Helvetica",20),fg="white",bg="#747474")
 timezone.place(x=600,y=20)
-
-#long_lat = Label (root,font=("Helvetica",20),fg="white",bg="#747474")
-#long_lat.place(x=600,y=50)
-
 
 
 mainloop()
